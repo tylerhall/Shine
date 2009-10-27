5d92c5cf0fd5230ac0918c8333d2714a843b4e99
Tyler Hall <tylerhall@clickontyler.com>
2009-10-27 09:13:05 -0500

    Adding MacUpdate Promo integration

---
+++ mupromo.php
@@ -0,0 +1,19 @@
+<?PHP
+    // This was code used to integrate with MUPromo.com
+
+    // require 'includes/master.inc.php';
+    // 
+    // $app = new Application(3);
+    // 
+    // $o              = new Order();
+    // $o->app_id      = $app->id;
+    // $o->dt          = dater();
+    // $o->type        = 'MUPromo';
+    // $o->first_name  = $_GET['first_name'];
+    // $o->last_name   = $_GET['last_name'];
+    // $o->payer_email = $_GET['email'];
+    // $o->insert();
+    // 
+    // $o->generateLicense();
+    // $o->emailLicense();
+    // echo "Ok";
