f34c44a306e117beb1538f172098ef12d00c9ef0
Tyler Hall <tylerhall@clickontyler.com>
2010-01-02 01:56:05 -0600

    Feedback tab will be highlighted if there is unread feedback

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,9 +1,17 @@
+<?PHP
+    $db = Database::getDatabase();
+    $feedback_count = $db->getValue("SELECT COUNT(*) FROM feedback WHERE new = 1");
+?>
 <h1>Shine</h1>
 <div id="navigation">
     <ul id="primary-navigation">
         <li class="active"><a href="index.php">Applications</a></li>
         <li><a href="orders.php">Orders</a></li>
+        <?PHP if($feedback_count == 0) : ?>
         <li><a href="feedback.php">Feedback</a></li>
+        <?PHP else : ?>
+        <li class="highlight"><a href="feedback.php">Feedback</a></li>
+        <?PHP endif; ?>
         <li><a href="stats.php">Sparkle Stats</a></li>
     </ul>
 
