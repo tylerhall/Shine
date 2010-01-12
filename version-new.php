9d7e7256c6b06bebfce239c9c6e48a5910c6fd65
Tyler Hall <tylerhall@clickontyler.com>
2010-01-12 10:55:06 -0600

    Fixed bug in sign_file(). Thanks, Jeff.

--- version-new.php
+++ version-new.php
@@ -53,7 +53,7 @@
     {
         $binary_hash = shell_exec('openssl dgst -sha1 -binary < ' . $filename);
         $hash_tmp_file = tempnam('/tmp', 'foo');
-        file_put_contents($tmp_file, $binary_hash);
+        file_put_contents($hash_tmp_file, $binary_hash);
 
         $key_tmp_file = tempnam('/tmp', 'bar');
         file_put_contents($key_tmp_file, "-----BEGIN DSA PRIVATE KEY-----\n" . $keydata . "\n-----END DSA PRIVATE KEY-----\n");
